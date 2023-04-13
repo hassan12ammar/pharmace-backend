3972a14acfc1eb02d7977ac497130e0476f6ea2d
hassan12ammar <mgamea125@gmail.com>
2023-04-13 03:16:23 +0300

    update urls.py to add routers

--- pharmace/urls.py
+++ pharmace/urls.py
@@ -1,15 +1,19 @@
 """ pharmace URL Configuration """
-from django.contrib import admin
-from django.urls import path
 from ninja import NinjaAPI
+from django.urls import path
+from django.contrib import admin
 # local models
+from core.controllers import pharmacy_router, cart_router
 from auth_profile.controllers import auth_controller, profile_controller
 
 
 api = NinjaAPI(title="pharmace Backend", 
                 description="Backend of E-commerc for pharmacies website using Django and Ninja")
 api.add_router("auth", auth_controller, tags=["Auth"])
-api.add_router("auth", profile_controller, tags=["Profile"])
+api.add_router("profile", profile_controller, tags=["Profile"])
+# 
+api.add_router("pharmacy", pharmacy_router, tags=["Pharmacy"])
+api.add_router("cart", cart_router, tags=["Cart"])
 
 urlpatterns = [
     path('admin/', admin.site.urls),
