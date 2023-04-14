7f4d9f222888457149636bc2a1646aaad378bf98
hassan12ammar <mgamea125@gmail.com>
2023-04-14 05:30:44 +0300

    implement E-commerce API

--- core/controllers.py
+++ core/controllers.py
@@ -1,3 +1,191 @@
+from typing import List
 from ninja import Router
+from rest_framework import status
+# locall models
+from .models import Cart, DrugItem, Pharmacy, Review, Drug
+from pharmace.utlize.custom_classes import Error
+from auth_profile.authentication import CustomAuth
+from pharmace.utlize.utlize import get_user_profile
+from .schemas import (CartOut, DrugItemOut, PharmacyOut, 
+                      PharmacyShort, MessageOut, ReviewOut)
+
+# 
+pharmacy_router = Router()
+cart_router = Router()
+
+
+""" Pharmacy """
+@pharmacy_router.get("get_all",
+                     response={
+                         200:List[PharmacyShort],
+                     })
+def get_all(request):
+    return status.HTTP_200_OK, Pharmacy.objects.all()
+
+
+@pharmacy_router.get("get_by_id/{id}",
+                     response={
+                         200:List[PharmacyOut],
+                         400: MessageOut
+                     })
+def get_by_id(request, id: int):
+    pharmacy = Pharmacy.objects.filter(id=id)
+    if not pharmacy:
+        return status.HTTP_400_BAD_REQUEST, MessageOut(detail=f"Pharmacy with id {id} Not Found")
+
+    return status.HTTP_200_OK, Pharmacy.objects.filter(id=id)
+
+
+@pharmacy_router.get("get_reviews/{id}",
+                     response={
+                         200:List[ReviewOut],
+                         400: MessageOut,
+                     },)
+def get_pharm_reviews(request, id: int):
+    pharmacy = Pharmacy.objects.filter(id=id).first()
+    if not pharmacy:
+        return status.HTTP_400_BAD_REQUEST, MessageOut(detail=f"Pharmacy with id {id} Not Found")
+
+    return Review.objects.filter(pharmacy=pharmacy)
+
+
+""" Cart """
+
+
+@cart_router.get("get_cart",
+                 response={200:CartOut},
+                 auth=CustomAuth(),)
+def get_cart(request):
+        # get the user from email in auth
+    email = request.auth
+
+    # get user profile
+    profile = get_user_profile(email)
+    if isinstance(profile, Error):
+        return profile.status, profile.message
+
+    cart = Cart.objects.filter(user=profile).first()
+    items = list(DrugItem.objects.filter(cart=cart))
+    total =  sum([
+            item.drug.price * item.amount
+            for item in items
+            ])
+    
+    result = cart.__dict__
+    result["items"] = items
+    result["user"] = profile
+    result["total"] = total
+
+    return status.HTTP_200_OK, result
+
+
+@cart_router.post("add_increment_to_cart/{drug_id}",
+                  response={
+                      200: DrugItemOut,
+                      400: MessageOut,
+                      404: MessageOut,
+                  },
+                auth=CustomAuth(),)
+def add_to_cart(request, drug_id: int):
+    # get the user from email in auth
+    email = request.auth
+
+    # check if user and profile exists
+    profile = get_user_profile(email)
+    if isinstance(profile, Error):
+        return profile.status, profile.message
+    # cart of the user
+    cart = Cart.objects.filter(user=profile).first()
+    drug = Drug.objects.filter(id=drug_id).first()
+
+    # check if there is an item
+    item = DrugItem.objects.filter(drug=drug,
+                                   cart=cart,)
+
+    if item.exists():
+        item = item.first()
+        item.amount += 1
+        item.save()
+
+        return item
+    
+    # create Item
+    item = DrugItem.objects.create(drug = drug,
+                                          cart=cart,
+                                          amount=1)
+
+    return item
+
+
+@cart_router.put("remove_from_cart/{drug_id}",
+                  response={
+                      200: DrugItemOut,
+                      400: MessageOut,
+                      404: MessageOut,
+                  },
+                auth=CustomAuth(),)
+def remove_from_cart(request, drug_id: int):
+    # get the user from email in auth
+    email = request.auth
+
+    # check if user and profile exists
+    profile = get_user_profile(email)
+    if isinstance(profile, Error):
+        return profile.status, profile.message
+
+    # cart of the user
+    cart = Cart.objects.filter(user=profile).first()
+    drug = Drug.objects.filter(id=drug_id).first()
+
+    # check if there is an item
+    item = DrugItem.objects.filter(drug=drug,
+                                   cart=cart,)
+
+    if item.exists():
+        item = item.first()
+        item.delete()
+
+        return status.HTTP_200_OK, MessageOut(detail="Item Deleted")
+
+    return status.HTTP_404_NOT_FOUND, MessageOut(detail="Item Not Found")
+
+
+
+@cart_router.put("decrease_from_cart/{drug_id}",
+                  response={
+                      200: DrugItemOut,
+                      400: MessageOut,
+                      404: MessageOut,
+                  },
+                auth=CustomAuth(),)
+def decrease_from_cart(request, drug_id: int):
+    # get the user from email in auth
+    email = request.auth
+
+    # check if user and profile exists
+    profile = get_user_profile(email)
+    if isinstance(profile, Error):
+        return profile.status, profile.message
+
+    # cart of the user
+    cart = Cart.objects.filter(user=profile).first()
+    drug = Drug.objects.filter(id=drug_id).first()
+
+    # check if there is an item
+    item = DrugItem.objects.filter(drug=drug,
+                                   cart=cart,)
+
+    if item.exists():
+        item = item.first()
+        item.amount -= 1
+        item.save()
+
+        if item.amount <= 0:
+            item.delete()
+            return status.HTTP_200_OK, MessageOut(detail="Item Deleted")
+
+        return item
+
+    return status.HTTP_404_NOT_FOUND, MessageOut(detail="Item Not Found")
 
 
---
+++ core/schemas.py
@@ -0,0 +1,156 @@
+from typing import List
+from ninja import Schema
+from datetime import date, time
+# local models
+from auth_profile.schemas import ProfileOut
+from core.models import Drug
+
+
+# General Schemas
+class MessageOut(Schema):
+    detail: str
+
+
+class ProfileReview(Schema):
+    name: str
+    img: str = None
+
+
+class DrugSchema(Schema):
+    name: str
+    description: str
+    img: str = None
+    price: float
+
+
+class DrugIn(DrugSchema):
+    pass
+
+
+class DrugOut(DrugSchema):
+    id: int
+
+
+class ReviewSchema(Schema):
+    user: ProfileReview
+    rating: float
+    description: str = None
+    start_date: date
+
+
+class ReviewIn(ReviewSchema):
+    pass
+
+
+class ReviewOut(ReviewSchema):
+    pass
+
+class OpeningHoursSchema(Schema):
+    weekday: str
+    from_hour: time
+    to_hour: time
+
+
+class PharmacyShort(Schema):
+    id: int
+    name: str
+    description: str
+    img: str
+    location: str
+
+    avg_stars: float
+
+    @staticmethod
+    def resolve_avg_stars(self):
+        reviews = self.review_set.all()
+        rates = [review.rating 
+                 for review in reviews]
+
+        return sum(rates) / len(rates)    
+
+
+class PharmacySchema(Schema):
+    name: str
+    description: str
+    img: str
+    location: str
+    drugs: List[DrugOut]
+
+    pct_rates: dict = None
+    opening_hours: List[OpeningHoursSchema]
+
+    @staticmethod
+    def resolve_opening_hours(self):
+        return self.opening_hours.all()
+
+    @staticmethod
+    def resolve_drugs(self):
+        return Drug.objects.filter(pharmacy=self)
+
+    @staticmethod
+    def resolve_pct_rates(self):
+        rates = [round(review.rating) 
+                 for review in self.review_set.all()]
+
+        if not rates:
+            return
+
+        counts = {i: rates.count(i) 
+                  for i in range(1, 5 +1)}
+        percentages = {i: round(counts[i] / len(rates) * 100) 
+                       for i in range(1, 6)}
+
+        return percentages
+
+
+class PharmacyIn(PharmacySchema):
+    pass
+
+
+class PharmacyOut(PharmacySchema):
+    id: int
+
+
+""" Cart Schemas """
+
+
+class DrugItemSchema(Schema):
+    amount: int
+
+
+class DrugItemIn(DrugItemSchema):
+    drug_id: int
+
+
+class DrugItemOut(DrugItemSchema):
+    drug: DrugOut
+    total: float
+
+    @staticmethod
+    def resolve_total(self):
+        return self.drug.price * self.amount
+
+
+class CartSchema(Schema):
+    user: ProfileOut
+    items: List[DrugItemOut]
+    ordered: bool
+    start_date: date
+    ordered_date: date
+
+    total: float
+
+    @staticmethod
+    def resolve_total(self):
+        return sum([
+            item.total
+            for item in self.items.all()
+        ])
+
+
+class CartIn(CartSchema):
+    pass
+
+
+class CartOut(CartSchema):
+    pass
