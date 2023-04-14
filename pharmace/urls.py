a8e99894ec2e55f05b54deea4abacca27bd52b34
hassan12ammar <mgamea125@gmail.com>
2023-04-14 21:14:45 +0300

    add description & draft router

--- pharmace/urls.py
+++ pharmace/urls.py
@@ -3,17 +3,19 @@ from ninja import NinjaAPI
 from django.urls import path
 from django.contrib import admin
 # local models
+from pharmace.utlize.constant import DESCRIPTION
 from core.controllers import pharmacy_router, cart_router
-from auth_profile.controllers import auth_controller, profile_controller
-
+from auth_profile.controllers import auth_controller, profile_controller, draft_router
 
 api = NinjaAPI(title="pharmace Backend", 
-                description="Backend of E-commerc for pharmacies website using Django and Ninja")
+                description=DESCRIPTION)
 api.add_router("auth", auth_controller, tags=["Auth"])
 api.add_router("profile", profile_controller, tags=["Profile"])
 # 
 api.add_router("pharmacy", pharmacy_router, tags=["Pharmacy"])
 api.add_router("cart", cart_router, tags=["Cart"])
+api.add_router("draft", draft_router, tags=["Draft"])
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- pharmace/utlize/constant.py
+++ pharmace/utlize/constant.py
@@ -8,3 +8,7 @@ load_dotenv()
 #Get specific environment variables
 SECRET_KEY = os.environ['SECRET_KEY']
 DEBUG = bool(os.environ['DEBUG'])
+# general constants
+DESCRIPTION="Pharmaceutical is a backend API for an e-commerce website for pharmacies. \
+    The API is built using Django and Django Ninja and allows users to perform various operations\
+        related to authentication, user profiles, pharmacies, drugs, and shopping cart management."
