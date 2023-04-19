1edf5df1df8f2eae9764cd2e93ac6fa50dc9720c
hassan12ammar <mgamea125@gmail.com>
2023-04-20 00:10:47 +0300

    update migration script to create seed after

--- migration.sh
+++ migration.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
+# remove old migrations
 find . -path "*/auth_profile/migrations/*.py" -not -name "__init__.py" -delete
 find . -path "*/core/migrations/*.py" -not -name "__init__.py" -delete
 
+# remove old DataBase
 rm db.sqlite3
 
-python manage.py makemigrations
+# Activate the virtual environment
+source pharmace_venv/bin/activate
 
+# make & apply new migrations
+python manage.py makemigrations
 python manage.py migrate
 
+# create superuser
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('mg@gm.com', 'string12')" | python manage.py shell
+
+# crate seed
+curl -X 'POST' \
+  'http://127.0.0.1:8000/api/draft/create_seed' \
+  -H 'accept: application/json' \
+  -d ''
