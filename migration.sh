4f37b2de52cb6ecab26c11937b6bb4aed2142818
hassan12ammar <mgamea125@gmail.com>
2023-04-20 04:58:53 +0300

    update automated scripts

--- migration.sh
+++ migration.sh
@@ -17,8 +17,32 @@ python manage.py migrate
 # create superuser
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('mg@gm.com', 'string12')" | python manage.py shell
 
+# Define a function to stop the server processes
+function stop_server() {
+    echo "Stopping server..."
+    # Find all processes listening on port 8000
+    pids=$(lsof -i :8000 | awk 'NR>1 {print $2}')
+    # Kill each process
+    for pid in $pids; do
+      echo "Killing process $pid..."
+      kill $pid
+    done
+    exit
+}
+
+# Run Django development server
+python manage.py runserver &
+
+# Wait for the server to start
+sleep 8
+
 # crate seed
 curl -X 'POST' \
   'http://127.0.0.1:8000/api/draft/create_seed' \
-  -H 'accept: application/json' \
-  -d ''
+  -H 'accept: application/json'y
+
+# Stop the server process on Ctrl+C
+trap stop_server SIGINT
+
+# Wait for the server to be stopped
+wait
--- start.sh
+++ start.sh
@@ -5,3 +5,6 @@ source pharmace_venv/bin/activate
 
 # Run the Django development server
 python manage.py runserver
+
+# Wait for the server to be stopped
+wait
